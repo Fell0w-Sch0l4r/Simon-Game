a13e9f440b0495562194e1a253349c22aa2ecc77
Ângel Cagiza <angelcagiza@gmail.com>
2024-05-03 18:25:02 +0100

    add Press Start 2P font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/js/app.js"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./index.html", "./src/js/app.js"],
+    theme: {
+        extend: {
+            fontFamily: {
+                press: ['"Press Start 2P"'],
+            },
+        },
+    },
+    plugins: [],
+};
 
